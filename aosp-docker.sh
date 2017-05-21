0c2327de594ab853093bf6ee520248fcdf5a41ca
Piotr Król <piotr.krol@3mdeb.com>
2017-05-22 00:05:56 +0200

    aosp-docker.sh: validate params and mount ccache
    Signed-off-by: Piotr Król <piotr.krol@3mdeb.com>

--- aosp-docker.sh
+++ aosp-docker.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-if [ "$#" -ne 1 ]; then
+if [ "$#" -ne 2 ]; then
     echo "Illegal number of parameters"
     exit
 fi
 
 docker run --rm -it \
 -v $1:/home/build/aosp \
--v $(dirname $SSH_AUTH_SOCK):$(dirname $SSH_AUTH_SOCK) -e SSH_AUTH_SOCK=$SSH_AUTH_SOCK  \
--v ~/.gitconfig:/home/build/.gitconfig \
+-v $2:/home/build/.ccache \
 3mdeb/aosp-docker /bin/bash
